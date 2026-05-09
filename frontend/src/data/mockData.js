9caad4cce96d192d79ae2bc32867a403b9564c5e
Harshada-k21 <harshadakadwane21@gmail.com>
2026-05-09 17:54:30 +0530

    Delete frontend/src directory

--- frontend/src/data/mockData.js
+++
@@ -1,5 +0,0 @@
-export const initialMachines = [
-  { id: "M1", temp: 70, vibration: 0.2, status: "RUNNING" },
-  { id: "M2", temp: 78, vibration: 0.4, status: "RUNNING" },
-  { id: "M3", temp: 85, vibration: 0.6, status: "WARNING" },
-];
